444e3cf9dca399501323eec60a7405de51945e3d
vlady.turato <vlady.turato@rawfish.com>
2020-06-12 12:12:26 +0200

    Comment UIWebView implementation

--- BlocksKitRawfish/UIKit/UIWebView+BlocksKit.h
+++ BlocksKitRawfish/UIKit/UIWebView+BlocksKit.h
@@ -4,28 +4,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
 /** Block callbacks for UIWebView.
 
  @warning UIWebView is only available on a platform with UIKit.
 */
 
-@interface UIWebView (BlocksKit)
+@interface WKWebView (BlocksKit)
 
 /** The block to be decide whether a URL will be loaded. 
  
  @warning If the delegate implements webView:shouldStartLoadWithRequest:navigationType:,
  the return values of both the delegate method and the block will be considered.
 */
-@property (nonatomic, copy, setter = bk_setShouldStartLoadBlock:) BOOL (^bk_shouldStartLoadBlock)(UIWebView *webView, NSURLRequest *request, UIWebViewNavigationType navigationType);
+@property (nonatomic, copy, setter = bk_setShouldStartLoadBlock:) BOOL (^bk_shouldStartLoadBlock)(WKWebView *webView, NSURLRequest *request, WKNavigationType navigationType);
 
 /** The block that is fired when the web view starts loading. */
-@property (nonatomic, copy, setter = bk_setDidStartLoadBlock:) void (^bk_didStartLoadBlock)(UIWebView *webView);
+@property (nonatomic, copy, setter = bk_setDidStartLoadBlock:) void (^bk_didStartLoadBlock)(WKWebView *webView);
 
 /** The block that is fired when the web view finishes loading. */
-@property (nonatomic, copy, setter = bk_setDidFinishLoadBlock:) void (^bk_didFinishLoadBlock)(UIWebView *webView);
+@property (nonatomic, copy, setter = bk_setDidFinishLoadBlock:) void (^bk_didFinishLoadBlock)(WKWebView *webView);
 
 /** The block that is fired when the web view stops loading due to an error. */
-@property (nonatomic, copy, setter = bk_setDidFinishWithErrorBlock:) void (^bk_didFinishWithErrorBlock)(UIWebView *webView, NSError *error);
+@property (nonatomic, copy, setter = bk_setDidFinishWithErrorBlock:) void (^bk_didFinishWithErrorBlock)(WKWebView *webView, NSError *error);
 
 @end
